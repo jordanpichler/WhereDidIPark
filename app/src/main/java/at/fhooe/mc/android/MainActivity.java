77643f26bf53d4e4d66b611db22a4f1cc8958caf
Jordan Pichler <Jordan@jordans-mbp.home>
2016-06-13 12:02:55 +0200

    Basic Design structure with buttons that already work with intents/activities

--- app/src/main/java/at/fhooe/mc/android/MainActivity.java
+++ app/src/main/java/at/fhooe/mc/android/MainActivity.java
@@ -1,13 +1,41 @@
 package at.fhooe.mc.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle _savedInstanceState) {
         super.onCreate(_savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = (Button) findViewById(R.id.main_where_is_car);
+        b.setOnClickListener(this);
+        b = (Button) findViewById(R.id.main_where_is_bike);
+        b.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View _v) {
+        switch(_v.getId())   {
+            case R.id.main_where_is_car: {
+                Intent i = new Intent(this, where_car.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            } break;
+            case R.id.main_where_is_bike: {
+                Intent i = new Intent(this, where_bike.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            } break;
+            default: {
+
+            } break;
+        }
     }
 }
