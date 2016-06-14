639ba5d517fdb837537ce9e54d9484a35fc6817f
Jordan Pichler <Jordan@Jordans-MacBook-Pro.local>
2016-06-14 12:41:53 +0200

    oops, forgot to add files and changes to previous commit ^^

--- app/src/main/java/at/fhooe/mc/android/where_car.java
+++ app/src/main/java/at/fhooe/mc/android/where_car.java
@@ -1,9 +1,13 @@
 package at.fhooe.mc.android;
 
+import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentActivity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,13 +15,28 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class where_car extends FragmentActivity{
+public class where_car extends FragmentActivity implements View.OnClickListener {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_where_car);
+
+        ImageView car_photo = (ImageView) findViewById(R.id.car_photo);
+        car_photo.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View _v) {
+        if (_v.getId() == R.id.car_photo) {
+            Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(i, REQUEST_IMAGE_CAPTURE);
+
+        }
+    }
+
 
     /*
     @Override
