537e0d174f00c7d070dd1946a24c3977ed7d2330
Jordan Pichler <Jordan@Jordans-MacBook-Pro.local>
2016-06-15 01:47:07 +0200

    commit with camera properties

--- app/src/main/java/at/fhooe/mc/android/fragment_car.java
+++ app/src/main/java/at/fhooe/mc/android/fragment_car.java
@@ -2,9 +2,9 @@ package at.fhooe.mc.android;
 
 import android.app.Fragment;
 import android.location.Location;
-import android.os.Bundle;
+import android.os.Bundle;/*
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.support.annotation.Nullable;*/
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -111,7 +111,7 @@ public class fragment_car extends Fragment implements OnMapReadyCallback, Google
 
 
     @Override
-    public void onConnected(@Nullable Bundle bundle) {
+    public void onConnected(/*@Nullable*/ Bundle bundle) {
         LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
     }
 
@@ -121,7 +121,7 @@ public class fragment_car extends Fragment implements OnMapReadyCallback, Google
     }
 
     @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    public void onConnectionFailed(/*@NonNull*/ ConnectionResult connectionResult) {
 
     }
 
