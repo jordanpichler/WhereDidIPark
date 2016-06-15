6ce7512a63799551d530c105f11b03494f7b55e0
Jordan Pichler <Jordan@Jordans-MacBook-Pro.local>
2016-06-15 18:23:01 +0200

    !!!! - Added functionality for bicycles !!!!

---
+++ app/src/main/java/at/fhooe/mc/android/fragment_bike.java
@@ -0,0 +1,180 @@
+package at.fhooe.mc.android;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class fragment_bike extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
+    MapView mView;
+    GoogleMap mMap;
+    LocationRequest mLocationRequest;
+    boolean zoom;
+    GoogleApiClient mGoogleApiClient;
+    public static Marker bikeLocation;
+    public static SharedPreferences sp;
+    public static String KEY_BIKE_LONGITUDE = "WhereDidIParkBIKEKEYlongi";
+    public static String KEY_BIKE_LATITUDE = "WhereDidIParkBIKEKEYlati";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate and return the layout
+        View v = inflater.inflate(R.layout.layout_fragment_bike, container, false);
+        mView = (MapView) v.findViewById(R.id.bikeMapView);
+
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(mView.getContext())
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+        mLocationRequest = new LocationRequest().setInterval(1000).setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mView.onCreate(savedInstanceState);
+        mView.getMapAsync(this);
+        return v;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mView.onLowMemory();
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        zoom = false;
+        mMap = googleMap;
+        mGoogleApiClient.connect();
+
+
+        sp = getActivity().getPreferences(Context.MODE_PRIVATE);
+
+        float lati = sp.getFloat(KEY_BIKE_LATITUDE, -1);
+        float longi = sp.getFloat(KEY_BIKE_LONGITUDE, -1);
+
+        MarkerOptions bikeOptions;
+
+        if (lati != -1 && longi != -1) {
+            zoom = true;
+            bikeOptions = new MarkerOptions()
+                    .position(new LatLng(lati, longi))
+                    .title("Bike Position");
+        } else {
+            bikeOptions = new MarkerOptions()
+                    .position(new LatLng(0, 0))
+                    .title("Bike Position");
+        }
+
+        bikeLocation = mMap.addMarker(bikeOptions);
+        bikeLocation.setVisible(false);
+        mMap.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onConnected(/*@Nullable*/ Bundle bundle) {
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(/*@NonNull*/ ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        float lati = sp.getFloat(KEY_BIKE_LATITUDE, -1);
+        float longi = sp.getFloat(KEY_BIKE_LONGITUDE, -1);
+
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+
+        if(lati == -1 && longi == -1) { // Not saved
+            bikeLocation.setVisible(false);
+            bikeLocation.setPosition(latLng);
+
+            if(!zoom) {
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18));
+                zoom = true;
+            }
+
+            else {
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            }
+        }
+
+        else { // saved
+            LatLngBounds.Builder latLngBuilder = new LatLngBounds.Builder();
+            latLngBuilder.include(new LatLng(lati, longi));
+            latLngBuilder.include(new LatLng(location.getLatitude(), location.getLongitude()));
+            LatLngBounds bounds = latLngBuilder.build();
+            mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 50));
+            bikeLocation.setVisible(true);
+        }
+    }
+}
--- app/src/main/java/at/fhooe/mc/android/where_bike.java
+++ app/src/main/java/at/fhooe/mc/android/where_bike.java
@@ -1,13 +1,202 @@
 package at.fhooe.mc.android;
 
-import android.app.Activity;
-import android.os.Bundle;
 
-public class where_bike extends Activity {
+import android.app.ActionBar;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class where_bike extends FragmentActivity implements View.OnClickListener {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public boolean SAVE;
+    private String TAG = "Where is my Bike";
+    private ImageView mBikeImage;
+    private EditText mNotes;
+    private Bitmap mBikeBitmap;
+    public String KEY_IMG_PATH = "WhereDidIParkBIKEBIKEImgPath";
+    public String KEY_NOTES = "WhereDidIParkBIKEBIKENotes";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_where_bike);
+
+        SAVE = false;
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setTitle("Your Bike");
+        actionBar.setHomeButtonEnabled(true);
+
+        Button bike_take_photo = (Button) findViewById(R.id.bike_take_photo);
+        bike_take_photo.setOnClickListener(this);
+
+        mBikeImage = (ImageView) findViewById(R.id.bike_photo);
+        mBikeImage.setOnClickListener(this);
+
+        mNotes = (EditText) findViewById(R.id.bike_notes);
+
+        //Recreate Saved Instance
+        SharedPreferences sp = getPreferences(Context.MODE_PRIVATE);
+
+        //--Image
+        String imgPath = sp.getString(KEY_IMG_PATH, null);
+        mBikeBitmap = loadImageFromStorage(imgPath);
+        mBikeImage.setImageBitmap(mBikeBitmap);
+
+        //--Notes
+        String notesContent = sp.getString(KEY_NOTES, null);
+        mNotes.setText(notesContent);
+    }
+
+    @Override
+    public void onClick(View _v) {
+        switch (_v.getId()) {
+            case R.id.bike_take_photo: {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+            break;
+            case R.id.bike_photo: {
+                //TODO: View FUll Size Image
+            }
+            break;
+            default: {
+                Log.e(TAG, "unexpected onClick ID encountered");
+            }
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_options, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_delete : {
+                fragment_bike.bikeLocation.setVisible(false);
+
+                SharedPreferences.Editor edit = fragment_bike.sp.edit();
+                edit.clear();
+                edit.commit();
+                Log.i(TAG, "cleared Shared Prefs");
+
+                mBikeImage.setImageBitmap(null);
+                mNotes.setText(null);
+
+
+            } break;
+
+            case R.id.action_save : {
+                SharedPreferences.Editor edit = fragment_bike.sp.edit();
+                edit.putFloat(fragment_bike.KEY_BIKE_LONGITUDE, (float) fragment_bike.bikeLocation.getPosition().longitude);
+                edit.putFloat(fragment_bike.KEY_BIKE_LATITUDE, (float) fragment_bike.bikeLocation.getPosition().latitude);
+                fragment_bike.bikeLocation.setVisible(true);
+
+                String imagePath = saveToInternalStorage(mBikeBitmap);
+                edit.putString(KEY_IMG_PATH, imagePath);
+
+                String notesContent = mNotes.getText().toString();
+                edit.putString(KEY_NOTES, notesContent);
+
+                Log.i(TAG, "saved to Shared Prefs");
+
+                edit.commit();
+                mBikeBitmap = null;
+                Toast.makeText(this, "All Saved! :)", Toast.LENGTH_SHORT).show();
+            } break;
+
+            default: {
+
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            mBikeBitmap = (Bitmap) extras.get("data");
+            mBikeImage.setImageBitmap(mBikeBitmap);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    private String saveToInternalStorage(Bitmap bitmapImage){
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        // Create imageDir
+        File mypath=new File(directory,"recoveredBikePhoto.jpg");
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Log.i(TAG, "created Path String for Photo (Shared Prefs)");
+        return directory.getAbsolutePath();
+    }
+
+    private Bitmap loadImageFromStorage(String path)
+    {
+        try {
+            File f = new File(path, "recoveredBikePhoto.jpg");
+            Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
+            Log.i(TAG, "retrieved Photo Path (Shared Prefs): " + path);
+            return b;
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+
     }
 }
