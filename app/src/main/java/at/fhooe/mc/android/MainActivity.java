7cd57fb38d74c964b9c536638a9e1d31c009ed05
EvaJobst <S1510237014@students.fh-hagenberg.at>
2016-06-15 21:34:36 +0200

    - Refactored stuff

--- app/src/main/java/at/fhooe/mc/android/MainActivity.java
+++ app/src/main/java/at/fhooe/mc/android/MainActivity.java
@@ -2,10 +2,8 @@ package at.fhooe.mc.android;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity implements View.OnClickListener {
@@ -26,12 +24,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
     public void onClick(View _v) {
         switch(_v.getId())   {
             case R.id.main_where_is_car: {
-                Intent i = new Intent(this, where_car.class);
+                Intent i = new Intent(this, ActivityCar.class);
                 i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(i);
             } break;
             case R.id.main_where_is_bike: {
-                Intent i = new Intent(this, where_bike.class);
+                Intent i = new Intent(this, ActivityBike.class);
                 i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(i);
             } break;
